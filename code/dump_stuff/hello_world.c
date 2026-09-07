4fce6a8697c91b6cbd9945cc58fab22c2b0d936a
John Szwakob III <szwakoj@sunypoly.edu>
2026-09-07 16:51:28 -0400

    Deployed 472a387 with MkDocs version: 1.6.1

---
+++ code/dump_stuff/hello_world.c
@@ -0,0 +1,14 @@
+// Hello World Program
+// Author: John Szwakob III
+
+/*
+Description:
+A program that says Hello World.
+*/
+
+#include <stdio.h>
+
+int main(){
+	printf("Hello World!");
+	return 0;
+}
